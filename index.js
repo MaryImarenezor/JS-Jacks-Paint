284903aa7738766ab01aeed6f53b28829e8f8454
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-19 23:48:24 +0000

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+use addEventListener("mouseover", function(){
+    insert funciton here
+    });
+
+    .append is for adding
+    .remove is for removing
+
+    access the alt from the html file
+
+    use data attributes if you want
+
+    e.target gives you the element your event was fired on
+    */
